4c09c7215fb06df52f780bd122f5380611f21989
John Ezekiel <blossomjohn399@gmail.com>
2024-08-30 17:05:18 +0100

    src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Home from "./components/Home";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 import Navbar from "./components/Navbar";
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
       <About />
       <Contact />
       <SpeedInsights />
+      <Analytics />
     </div>
   );
 }
